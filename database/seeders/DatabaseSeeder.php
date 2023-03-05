b9822541c3c080d9fa73f65b3a82ef5c9a91e983
mhdky <83162131+mhdky@users.noreply.github.com>
2023-03-05 21:54:53 +0700

    hapus data databaseseeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
-use App\Models\Tenant;
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,15 +12,5 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Muhammad Rizki',
-            'email' => 'mhdky502@gmail.com',
-            'password' => Hash::make('password')
-        ]);
-        Tenant::create([
-            'name' => 'effenril agung',
-            'status' => 'Kontrakan',
-            'url' => uniqid()
-        ]);
     }
 }
